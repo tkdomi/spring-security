af14b1d4029bf577f8618a77299f9880b6ee50a7
Dominik Kaczorowski <kaczorowski.dominik@gmail.com>
2021-02-11 19:53:23 +0100

    Init project

---
+++ src/test/java/pl/hqbusiness/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hqbusiness.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
